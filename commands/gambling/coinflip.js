befb8d193f9c25080cc44e86bade703aad53b544
S X A DEVELOPMENT <90172970+samirop12@users.noreply.github.com>
2021-09-25 02:38:44 +0000

    cf

---
+++ commands/gambling/coinflip.js
@@ -0,0 +1,10 @@
+const Discord = require("discord.js")
+module.exports = {
+  name: 'coinflip',
+  aliases: ['cf'] ,
+  description: 'to do a coinflip',
+  usage: 'prefix+coinflip',
+  run : async(client, message, args) => {
+ 
+  }
+}
